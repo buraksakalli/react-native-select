5231cec23e2e6726dbc87b97619a340ce9f623d3
Burak Sakalli <buraksakalli06@gmail.com>
2022-10-03 00:32:33 +0400

    feature: folder structure update. Move styles into style.ts

---
+++ src/components/Button/button.style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  button: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 3,
+    padding: 5,
+    margin: 5,
+    flexDirection: 'row',
+    borderWidth: 1,
+  },
+});
